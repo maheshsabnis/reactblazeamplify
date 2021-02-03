47f1982ddb4bc790d60ef4f27787fc6c8c250bf7
Mahesh Sabnis <sabnis_m@hotmail.com>
2021-02-03 15:23:43 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 function App() {
+  const [emp,update] = useState({EmpNo:0,EmpName:'',DeptName:''});
+  const [emps, add] = useState([]);
+
+  useEffect(()=>{
+    axios.get("https://v0yip3ygbb.execute-api.ap-south-1.amazonaws.com/production/api/employees")
+        .then((resp)=> {
+          add(resp.data);
+        }).catch((error)=>{
+          console.log(`Error Occured ${error.message}`);
+        });
+  },[]);
+
+  const save=()=>{
+    // add([...emps, {
+    //   EmpNo:emp.EmpNo, EmpName:emp.EmpName, DeptName: emp.DeptName
+    // }]);
+    const e = {
+       EmpNo: emp.EmpNo, EmpName: emp.EmpName, DeptName: emp.DeptName
+    };
+    axios.post("https://v0yip3ygbb.execute-api.ap-south-1.amazonaws.com/production/api/employees", e, {
+      headers: {
+        "Content-Type":"application/json"
+      }
+    })
+    .then((resp)=> {
+      add(resp.data);
+    }).catch((error)=>{
+      console.log(`Error Occured ${error.message}`);
+    });
+  };
+
+  const clear=()=>{
+    update({EmpNo:0,EmpName:'',DeptName:''});
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <table>
+          <tr>
+            <td>
+              EmpNo
+            </td>
+            <td>
+              <input type="text" value={emp.EmpNo}
+               onChange={(evt)=> {update({...emp, EmpNo:parseInt(evt.target.value)})}}/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              EmpName
+            </td>
+            <td>
+              <input type="text" value={emp.EmpName}
+              onChange={(evt)=> {update({...emp, EmpName:evt.target.value})}}/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              DeptName
+            </td>
+            <td>
+              <input type="text" value={emp.DeptName}
+              onChange={(evt)=> {update({...emp, DeptName:evt.target.value})}}/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="button" value="Save" onClick={save}/>
+            </td>
+            <td>
+              <input type="button" value="Clear" onClick={clear}/>
+            </td>
+          </tr>
+        </table>
+        <hr/>
+        <div>
+          Received  Data
+          <br/>
+          {JSON.stringify(emps)}
+        </div>
     </div>
   );
 }
